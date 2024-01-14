966ab886d66f957ee4ba48e1f72975b33ad7f967
Stefan <decimelli@ibm.com>
2024-01-14 16:34:14 -0500

    rm parts from asset dir

--- combineGeoData.sh
+++ combineGeoData.sh
@@ -1,4 +1,5 @@
-jq 'reduce .features[] as $f ({}; .[$f.properties.ADMIN] = $f.properties)' src/assets/countryNames.json > temp_props.json
-jq --slurpfile props temp_props.json '.features |= map(.properties |= . + ($props[0][.ADMIN] // {}))' src/assets/countryLines.json > temp2_props.json
+mkdir -p src/assets/
+jq 'reduce .features[] as $f ({}; .[$f.properties.ADMIN] = $f.properties)' geojson/countryNames.json > temp_props.json
+jq --slurpfile props temp_props.json '.features |= map(.properties |= . + ($props[0][.ADMIN] // {}))' geojson/countryLines.json > temp2_props.json
 jq -c . temp2_props.json > src/assets/countries.json
 rm temp_props.json temp2_props.json
