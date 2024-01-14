6faeed1d41095c64de34ffc5a5d6271dd04322b4
Stefan <decimelli@ibm.com>
2024-01-14 14:17:35 -0500

    add types

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,32 @@ import style from "./App.module.css"
 import 'leaflet/dist/leaflet.css';
 import { features } from "./assets/geodata.json"
 import { GeoJsonObject } from 'geojson';
-import { useMemo, useState } from 'react';
+import { ChangeEvent, useMemo, useState } from 'react';
 import L from 'leaflet';
 
 function App() {
-  const [selectedCountries, setSelectedCountries] = useState([]);
+
+  const [selectedCountries, setSelectedCountries] = useState<GeoJsonObject[]>([]);
   const [inputValue, setInputValue] = useState("");
 
-  const countryNames = useMemo(() => features.map(country => {
-    return new L.GeoJSON(country, {name: country.properties.name.toLowerCase()})
-  }), [])
+  const countryNames = useMemo(() => features.map(country => ({
+    names: new Set([
+      country.properties.name.toLowerCase(),
+    ]),
+    co: new L.GeoJSON(country as GeoJsonObject)
+  })), [])
 
   const [x, setX] = useState(51.505);
   const [y, setY] = useState(0);
 
 
-  const handleInput = async (e) => {
+  const handleInput = async (e: ChangeEvent<HTMLInputElement>) => {
     const countryGuess = e.target.value;
-    const i = countryNames.findIndex(c => c.options?.name === countryGuess.toLowerCase());
+    const i = countryNames.findIndex(c => c.names.has(countryGuess.toLowerCase()));
     if (i >= 0) {
-      setSelectedCountries([...selectedCountries, features[i]]);
-      setX(countryNames[i].getBounds().getCenter().lat)
-      setY(countryNames[i].getBounds().getCenter().lng)
+      setSelectedCountries([...selectedCountries, features[i] as GeoJsonObject]);
+      setX(countryNames[i].co.getBounds().getCenter().lat)
+      setY(countryNames[i].co.getBounds().getCenter().lng)
       setInputValue("");
     }
     else {
@@ -32,12 +36,10 @@ function App() {
     }
   }
 
-  console.log(selectedCountries)
-
   return (
     <div>
       <div className={style.mapContainer}>
-        <MapContainer center={[x, y]} zoom={4} scrollWheelZoom={false} className={style.map} key={`${x}${y}`}>
+        <MapContainer center={[x, y]} zoom={4} className={style.map} key={`${x}${y}`}>
           <TileLayer
             url="http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png"
             attribution="&copy;OpenStreetMap, &copy;CartoDB"
