9a32378359b339c368bd049c6b8024abe995aba7
Stefan <decimelli@ibm.com>
2024-01-14 22:14:43 -0500

    filter some country data

--- combineGeoData.sh
+++ combineGeoData.sh
@@ -1,5 +1,8 @@
 mkdir -p src/assets/
 jq 'reduce .features[] as $f ({}; .[$f.properties.ADMIN] = $f.properties)' geojson/countryNames.json > temp_props.json
-jq --slurpfile props temp_props.json '.features |= map(.properties |= . + ($props[0][.ADMIN] // {}))' geojson/countryLines.json > temp2_props.json
+jq --slurpfile props temp_props.json \
+   '.features |= map(select(.geometry != null) | .properties |= . + ($props[0][.ADMIN] // {}))' \
+   geojson/countryLines.json > temp2_props.json
+
 jq -c . temp2_props.json > src/assets/countries.json
 rm temp_props.json temp2_props.json
